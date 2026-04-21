ef997278f582854f3bd88e5ac2fb1af402188b73
Ghislain LEBLANC <ghislain.leblanc@mediactivegroup.com>
2026-04-21 13:55:33 +0200

    check steps

--- recovery_box_install.sh
+++ recovery_box_install.sh
@@ -8,8 +8,11 @@
 # monter /data/ avant de lancer le script
 #
 
-SRVMSG='\033[0;32m =+= '
-NC='\033[0m'
+SRVMSG=' =+= '
+MSGGREEN='\033[0;32m'
+MSGYELLOW='\033[0;33m'
+MSGRED='\033[0;31m'
+MSGNC='\033[0m'
 
 #######################################################
 # checks / settings
@@ -17,58 +20,64 @@ NC='\033[0m'
 
 #check if root
 if [[ $(whoami) != root ]]; then 
-    echo -e "$SRVMSG" "user is not root" ${NC}
+    echo -e "$MSGRED" "$SRVMSG" "user is not root" "$MSGNC"
     exit 1
 fi
 # check if /data exists
 if [[ ! -d /data ]]; then
-    echo -e "$SRVMSG" "/data does not exist. Please mount/create /data" ${NC}
-    echo -e "$SRVMSG" "example: mount /dev/sda1 /data" ${NC}
+    echo -e "$MSGRED" "$SRVMSG" "/data does not exist. Please mount/create /data" "$MSGNC"
+    echo -e "$MSGRED" "$SRVMSG" "example: mount /dev/sda1 /data" "$MSGNC"
     exit 1
 fi
 # check if we are on a debian system
 if [[ ! -f /etc/debian_version ]]; then
-    echo -e "$SRVMSG" "This script is only for Debian based systems" ${NC}
+    echo -e "$MSGRED" "$SRVMSG" "This script is only for Debian based systems" "$MSGNC"
     exit 1
 fi
 # check if we are on amd64 architecture
 if [[ $(dpkg --print-architecture) != "amd64" ]]; then
-    echo -e "$SRVMSG" "This script is only for amd64 architecture" ${NC}
+    echo -e "$MSGRED" "$SRVMSG" "This script is only for amd64 architecture" "$MSGNC"
     exit 1
 fi
 
 # language settings
-echo -e "$SRVMSG" "choose your language / choisissez votre langue :" ${NC}
-echo -e "$SRVMSG" "1 = English" ${NC}
-echo -e "$SRVMSG" "2 = Français" ${NC}
-echo -e "$SRVMSG" "3 = Tout/Both" ${NC}
-read -p "$SRVMSG Enter your choice / Entrez votre choix : " lang_choice
+echo -e "$MSGYELLOW" "$SRVMSG" "choose your language / choisissez votre langue :" "$MSGNC"
+echo -e "$MSGYELLOW" "$SRVMSG" "1 = English" "$MSGNC"
+echo -e "$MSGYELLOW" "$SRVMSG" "2 = Français" "$MSGNC"
+echo -e "$MSGYELLOW" "$SRVMSG" "3 = Tout/Both" "$MSGNC"
+read -r -p "$MSGYELLOW" "$SRVMSG Enter your choice / Entrez votre choix : " lang_choice
 case $lang_choice in
-    1) echo -e "$SRVMSG" "Language set to English" ${NC}
+    1) echo -e "$MSGGREEN" "$SRVMSG" "Language set to English" "$MSGNC"
     Lang="en"
     ;;
-    2) echo -e "$SRVMSG" "Langue définie sur Français" ${NC}
+    2) echo -e "$MSGGREEN" "$SRVMSG" "Langue définie sur Français" "$MSGNC"
     Lang="fr"
     ;;
-    3) echo -e "$SRVMSG" "Language set to English and Français" ${NC}
+    3) echo -e "$MSGGREEN" "$SRVMSG" "Language set to English and Français" "$MSGNC"
     Lang="all"
     ;;
-    *) echo -e "$SRVMSG" "Invalid choice, defaulting to French" ${NC}
+    *) echo -e "$MSGRED" "$SRVMSG" "Invalid choice, defaulting to French" "$MSGNC"
     Lang="fr"
     ;;esac
 
 #######################################################
 # Install basic tools
 #######################################################
-echo -e "$SRVMSG" "Installing basic tools..." ${NC}
+echo -e "$MSGYELLOW" "$SRVMSG" "Installing basic tools..." "$MSGNC"
 apt-get update -qq
 apt-get install -y -qq curl gpg ca-certificates git > /dev/null
+if [ $? -eq 0 ]; then
+    echo -e "$MSGGREEN" "$SRVMSG" "basic tools installed successfully.${MSGNC}"
+else
+    echo -e "$MSGRED" "$SRVMSG" "basic tools installation failed.${MSGNC}"
+    exit 1
+fi
 
 
 #######################################################
 # Add needed repositories
 #######################################################
-echo -e "$SRVMSG" "Adding repositories..." ${NC}
+echo -e "$MSGYELLOW" "$SRVMSG" "Adding repositories..." "$MSGNC"
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -81,6 +90,11 @@ Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
+if [ $? -ne 0 ]; then
+    echo -e "$MSGRED" "$SRVMSG" "failed to add Docker repository.${MSGNC}"
+    exit 1
+fi
+
 curl -fsSL https://download.opensuse.org/repositories/home:/tumic:/GPXSee/Debian_13/Release.key -o /etc/apt/keyrings/gpxsee.asc
 chmod a+r /etc/apt/keyrings/gpxsee.asc
 tee /etc/apt/sources.list.d/gpxsee.sources > /dev/null <<EOF
@@ -91,50 +105,102 @@ Components:
 Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/gpxsee.asc
 EOF
+
+if [ $? -ne 0 ]; then
+    echo -e "$MSGRED" "$SRVMSG" "failed to add GPXSee repository.${MSGNC}"
+    exit 1
+fi
+
 apt-get update -qq
+if [ $? -eq 0 ]; then
+    echo -e "$MSGGREEN" "$SRVMSG" "repository added successfully.${MSGNC}"
+else
+    echo -e "$MSGRED" "$SRVMSG" "failed to add repository.${MSGNC}"
+    exit 1
+fi
 
 #######################################################
 # install Docker
 #######################################################
-echo -e "$SRVMSG" "Installing Docker..." ${NC}
+echo -e "$MSGYELLOW" "$SRVMSG" "Installing Docker..." "$MSGNC"
 sudo apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null
-
+if [ $? -eq 0 ]; then
+    echo -e "$MSGGREEN" "$SRVMSG" "Docker installed successfully.${MSGNC}"
+else
+    echo -e "$MSGRED" "$SRVMSG" "failed to install Docker.${MSGNC}"
+    exit 1
+fi
 #######################################################
 # kiwix installation
 #######################################################
-echo -e "$SRVMSG" "Installing kiwix..." ${NC}
+echo -e "$MSGYELLOW" "$SRVMSG" "Installing kiwix..." "$MSGNC"
 
 docker pull ghcr.io/kiwix/kiwix-serve:3.8.2
+if [ $? -eq 0 ]; then
+    echo -e "$MSGGREEN" "$SRVMSG" "Kiwix installed successfully.${MSGNC}"
+else
+    echo -e "$MSGRED" "$SRVMSG" "failed to install Kiwix.${MSGNC}"
+    exit 1
+fi
+
 # Download Wikipedia for kiwix
 mkdir /data/kiwix
 if [[ "$Lang" == "fr" ]] || [[ "$Lang" == "all" ]]; then
-    echo -e "$SRVMSG" "Downloading Wikipedia in French. This step may take some time..." ${NC}
+    echo -e "$MSGYELLOW" "$SRVMSG" "Downloading Wikipedia in French. This step may take some time..." "$MSGNC"
     FileName=$(curl -s "https://download.kiwix.org/zim/wikipedia/" | grep -oP 'wikipedia_fr_all_nopic_\d{4}-\d{2}\.zim' | sort -V | tail -1)
     wget -q --show-progress -P /data/kiwix https://download.kiwix.org/zim/wikipedia/${FileName}
-fi
-if [[ "$Lang" == "en" ]] || [[ "$Lang" == "all" ]]; then
-    echo -e "$SRVMSG" "Downloading Wikipedia in English. This step may take some time..." ${NC}
-    FileName=$(curl -s "https://download.kiwix.org/zim/wikipedia/" | grep -oP 'wikipedia_en_all_nopic_\d{4}-\d{2}\.zim' | sort -V | tail -1)
-    wget -q --show-progress -P /data/kiwix https://download.kiwix.org/zim/wikipedia/${FileName}
+    if [[ -e /data/kiwix/$FileName ]]; then
+        echo -e "$MSGGREEN" "$SRVMSG" "Wikipedia in French downloaded successfully.${MSGNC}"
+    else
+        echo -e "$MSGRED" "$SRVMSG" "failed to download Wikipedia in French.${MSGNC}"
+        exit 1
+    fi
 fi
 
+if [[ "$Lang" == "en" ]] || [[ "$Lang" == "all" ]]; then
+    echo -e "$MSGYELLOW" "$SRVMSG" "Downloading Wikipedia in English. This step may take some time..." "$MSGNC"
+    FileName=$(curl -s "https://download.kiwix.org/zim/wikipedia/" | grep -oP 'wikipedia_en_all_nopic_\d{4}-\d{2}\.zim' | sort -V | tail -1)
+    wget -q --show-progress -P /data/kiwix https://download.kiwix.org/zim/wikipedia/${FileName}
+    if [[ -e /data/kiwix/$FileName ]]; then
+        echo -e "$MSGGREEN" "$SRVMSG" "Wikipedia in English downloaded successfully.${MSGNC}"
+    else
+        echo -e "$MSGRED" "$SRVMSG" "failed to download Wikipedia in English.${MSGNC}"
+        exit 1
+    fi
+fi
+
+
+
 # kiwix service creation
-echo -e "$SRVMSG" "Creating kiwix service..." ${NC}
+echo -e "$MSGYELLOW" "$SRVMSG" "Creating kiwix service..." "$MSGNC"
 cp assets/kiwix.service /etc/systemd/system/kiwix.service
 systemctl enable kiwix
 systemctl start kiwix
+if [[ $(systemctl is-active kiwix) == "active" ]]; then
+    echo -e "$MSGGREEN" "$SRVMSG" "Kiwix service started successfully.${MSGNC}"
+else
+    echo -e "$MSGRED" "$SRVMSG" "failed to start Kiwix service.${MSGNC}"
+    exit 1
+fi
 
 #######################################################
 # Access Point
 #######################################################
 
 # Disable wpa_supplicant on wlan0
-echo -e "$SRVMSG" "WiFi Access Point - preparing wlan0 interface..." ${NC}
-systemctl stop wpa_supplicant@wlan0
-systemctl disable wpa_supplicant@wlan0
+# should be useless with x64
+#echo -e "$SRVMSG" "WiFi Access Point - preparing wlan0 interface..." "$MSGNC"
+#systemctl stop wpa_supplicant@wlan0
+#systemctl disable wpa_supplicant@wlan0
+#if [[ $(systemctl is-active wpa_supplicant@wlan0) == "inactive" ]] && [[ $(systemctl is-enabled wpa_supplicant@wlan0) == "disabled" ]]; then
+#    echo -e "$MSGGREEN" "$SRVMSG" "wpa_supplicant disabled on wlan0 successfully.${MSGNC}"
+#else
+#    echo -e "$MSGRED" "$SRVMSG" "failed to disable wpa_supplicant on wlan0.${MSGNC}"
+#    exit 1
+#fi
 
 # Install the simple-pi-hotspot container
-echo -e "$SRVMSG" "WiFi Access Point - Installing simple-pi-hotspot container..." ${NC}
+echo -e "$MSGYELLOW""$SRVMSG" "WiFi Access Point - Installing simple-pi-hotspot container..." "$MSGNC"
 docker pull mrdgidgi/simple-pi-hotspot
 mkdir -p /etc/ap_config/
 cp assets/dnsmasq.conf /etc/ap_config/dnsmasq.conf
@@ -145,13 +211,26 @@ cp assets/ap.service /etc/systemd/system/ap.service
 systemctl daemon-reload
 systemctl enable ap.service
 systemctl start ap.service
+if [[ $(systemctl is-active ap.service) == "active" ]]; then
+    echo -e "$MSGGREEN" "$SRVMSG" "Access Point service started successfully.${MSGNC}"
+else
+    echo -e "$MSGRED" "$SRVMSG" "failed to start Access Point service.${MSGNC}"
+    exit 1
+fi
 
 # enable ipv4 routing
-echo -e "$SRVMSG" "Enabling IPv4 routing..." ${NC}
+echo -e "$MSGYELLOW""$SRVMSG" "Enabling IPv4 routing..." "$MSGNC"
 sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
+sysctl -p > /dev/null
+if [[ $(sysctl -n net.ipv4.ip_forward) -eq 1 ]]; then
+    echo -e "$MSGGREEN" "$SRVMSG" "IPv4 routing enabled successfully.${MSGNC}"
+else
+    echo -e "$MSGRED" "$SRVMSG" "failed to enable IPv4 routing.${MSGNC}"
+    exit 1
+fi
 
 # Setup IPtables
-echo -e "$SRVMSG" "Setting up IPtables for NAT and routing..." ${NC}
+echo -e "$MSGYELLOW" "$SRVMSG" "Setting up IPtables for NAT and routing..." "$MSGNC"
 mkdir -p /etc/iptables
 cp assets/iptables.sh /etc/iptables/iptables.sh
 chmod +x /etc/iptables/iptables.sh
@@ -159,18 +238,23 @@ cp assets/iptables.service /etc/systemd/system/iptables.service
 systemctl daemon-reload
 systemctl enable iptables.service
 systemctl start iptables.service
+if [[ $(systemctl is-active iptables.service) == "active" ]]; then
+    echo -e "$MSGGREEN" "$SRVMSG" "IPtables service started successfully.${MSGNC}"
+else
+    echo -e "$MSGRED" "$SRVMSG" "failed to start IPtables service.${MSGNC}"
+    exit 1
+fi
 
 #######################################################
 ## Apache
 #######################################################
 
 if [[ "$Lang" == "en" ]] || [[ "$Lang" == "all" ]]; then
-    echo -e "$SRVMSG" "Downloading English survival PDFs..." ${NC}
+    echo -e "$MSGYELLOW" "$SRVMSG" "Downloading English survival PDFs..." "$MSGNC"
     TPDir="/data/enpdf"
     mkdir -p "$TPDir"
-    cd "$TPDir" || exit
     curl -sL "https://trueprepper.com/survival-pdfs-downloads/" | grep -oP 'https?://[^"]+\.pdf' | sort -u > pdf_list.txt
-    wget -i pdf_list.txt -A pdf -nc -nv --wait=1 --random-wait
+    wget -P"$TPDir" -i pdf_list.txt -A pdf -nc -nv --wait=1 --random-wait 
     cat <<EOF > "$INDEX_FILE"
 <!DOCTYPE html>
 <html lang="fr">
@@ -202,26 +286,40 @@ EOF
 </body>
 </html>
 EOF
+    rm -y pdf_list.txt
+    if [[ -z $(ls -A "$TPDir"/*.pdf) ]]; then
+        echo -e "$MSGRED" "$SRVMSG" "failed to download English survival PDFs.${MSGNC}"
+        exit 1
+    else
+        echo -e "$MSGGREEN" "$SRVMSG" "English survival PDFs downloaded successfully.${MSGNC}"
+    fi
 
 fi
 
 # dump oldu.fr --- Site DOWN !!! forum toujours up + blog aussi. Voir pour récupérer les données du site depuis archive.org si problème persistant
-wget -P /data/ -mkxKE -e robots=off http://oldu.fr/
+#wget -P /data/ -mkxKE -e robots=off http://oldu.fr/
 # This step take a lot of time as we dump all the website
 
 # dump nopanic.fr
 if [[ "$Lang" == "fr" ]] || [[ "$Lang" == "all" ]]; then
-    echo -e "$SRVMSG" "Downloading nopanic.fr website..." ${NC}
+    echo -e "$MSGYELLOW" "$SRVMSG" "Downloading nopanic.fr website..." "$MSGNC"
     wget -P /data/nopanic -r -l 1 -nd -A pdf,html -H -p -k -e robots=off "https://nopanic.fr/bookbank/"
+    if [[ $? -eq 0 ]]; then
+        echo -e "$MSGGREEN" "$SRVMSG" "nopanic.fr website downloaded successfully.${MSGNC}"
+        
+    else
+        echo -e "$MSGRED" "$SRVMSG" "failed to download nopanic.fr website.${MSGNC}"
+        exit 1
+    fi
 fi
 
 # install + conf apache2
-echo -e "$SRVMSG" "Installing and configuring Apache2..." ${NC}
+echo -e "$MSGYELLOW" "$SRVMSG" "Installing and configuring Apache2..." "$MSGNC"
 apt-get install -y -qq apache2 > /dev/null
 if [[ "$Lang" == "fr" ]] || [[ "$Lang" == "all" ]]; then
     cp assets/enpdf.conf /etc/apache2/sites-available/enpdf.conf
     a2ensite enpdf.conf
-    echo -e "$SRVMSG" "pdf.recovery.box enabled" ${NC}
+    echo -e "$MSGGREEN" "$SRVMSG" "pdf.recovery.box enabled" "$MSGNC"
 fi
 if [[ "$Lang" == "fr" ]] || [[ "$Lang" == "all" ]]; then
     #cp assets/oldu.fr.conf /etc/apache2/sites-available/oldu.fr.conf
@@ -229,19 +327,26 @@ if [[ "$Lang" == "fr" ]] || [[ "$Lang" == "all" ]]; then
     #a2ensite oldu
     #echo "$SRVMSG" "oldu.recovery.box enabled"
     a2ensite nopanic
-    echo -e "$SRVMSG" "nopanic.recovery.box enabled" ${NC}
+    echo -e "$MSGGREEN" "$SRVMSG" "nopanic.recovery.box enabled" "$MSGNC"
 fi
 a2dissite 000-default
 sed -i 's/Listen 80/ Listen 8080/' /etc/apache2/ports.conf
 systemctl restart apache2
+if [[ $(systemctl is-active apache2) == "active" ]]; then
+    echo -e "$MSGGREEN" "$SRVMSG" "Apache2 configured successfully.${MSGNC}"
+else
+    echo -e "$MSGRED" "$SRVMSG" "failed to configure Apache2.${MSGNC}"
+    exit 1
+fi
 
 #######################################################
 # Mapping / navigation tools
 #######################################################
 
+## Headless server, no need for visual APP
 # Install gpxsee
-echo -e "$SRVMSG" "Installing GPXSee..." ${NC}
-apt-get install gpxsee -y -qq > /dev/null
+#echo -e "$SRVMSG" "Installing GPXSee..." "$MSGNC"
+#apt-get install gpxsee -y -qq > /dev/null
 # TODO default map load => opentopomap
 # sans doute dans /usr/share/gpxsee/maps
 
@@ -250,16 +355,22 @@ apt-get install gpxsee -y -qq > /dev/null
 #######################################################
 
 # Install OpenWebRX Plus
-echo -e "$SRVMSG" "Installing OpenWebRX Plus..." ${NC}
+echo -e "$MSGYELLOW" "$SRVMSG" "Installing OpenWebRX Plus..." "$MSGNC"
 mkdir -p /etc/owrx/var /etc/owrx/etc /etc/owrx/plugins/{receiver,map}
 docker pull slechev/openwebrxplus-softmbe:latest
 cp assets/openwebrx.service /etc/systemd/system/openwebrx.service
 systemctl daemon-reload
 systemctl enable openwebrx.service
 systemctl start openwebrx.service
+if [[ $(systemctl is-active openwebrx) == "active" ]]; then
+    echo -e "$MSGGREEN" "$SRVMSG" "OpenWebRX Plus service started successfully.${MSGNC}"
+else
+    echo -e "$MSGRED" "$SRVMSG" "failed to start OpenWebRX Plus service.${MSGNC}"
+    exit 1
+fi
 
 # Install the last driver for the rtl-sdr 
-echo -e "$SRVMSG" "Managing rtl-sdr drivers..." ${NC}
+echo -e "$MSGYELLOW" "$SRVMSG" "Managing rtl-sdr drivers..." "$MSGNC"
 apt-get purge rtl-sdr -y
 apt-get purge -y ^librtlsdr
 rm -rvf /usr/lib/librtlsdr* 
@@ -282,4 +393,4 @@ ldconfig
 #######################################################
 # Final message
 #######################################################
-echo -e "$SRVMSG" "Installation complete! Please reboot the system to apply all changes." ${NC}
+echo -e "$SRVMSG" "Installation complete! Please reboot the system to apply all changes." "$MSGNC"
